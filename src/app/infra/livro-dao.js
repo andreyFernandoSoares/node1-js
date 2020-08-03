004fe4f0f8d16a53ff1afe044ca047368e6e2ba6
diddyzin <andreysykez19@gmail.com>
2020-08-03 16:48:44 -0300

    Add

---
+++ src/app/infra/livro-dao.js
@@ -0,0 +1,48 @@
+const { ___resolveComponentKey } = require("marko/src/components/renderer");
+
+class LivroDao {
+    constructor(db){
+        this._db = db;
+    }
+
+    adiciona(livro){
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO LIVROS (
+                        titulo,
+                        preco,
+                        descricao
+                    ) values (?, ?, ?)`,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+    
+                    resolve();
+                } 
+            )
+        });
+    }
+
+    lista() {
+        return new Promise((resolve, reject) => {
+            this._db.all(
+                'SELECT * FROM livros',
+                (erro, resultados) => {
+                    if (erro) return reject('Não foi possível listar os livros!');
+
+                    return resolve(resultados);
+                }
+            )
+
+        });
+    }
+}
+
+module.exports = LivroDao;
