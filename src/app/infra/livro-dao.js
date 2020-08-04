f14d249302648b01226e9d1bd6a151fc76f27df2
diddyzin <andreysykez19@gmail.com>
2020-08-04 07:55:21 -0300

    Feito

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -1,18 +1,18 @@
-const { ___resolveComponentKey } = require("marko/src/components/renderer");
-
 class LivroDao {
-    constructor(db){
+
+    constructor(db) {
         this._db = db;
     }
 
-    adiciona(livro){
+    adiciona(livro) {
         return new Promise((resolve, reject) => {
             this._db.run(`
-                INSERT INTO LIVROS (
-                        titulo,
-                        preco,
-                        descricao
-                    ) values (?, ?, ?)`,
+                INSERT INTO livros (
+                    titulo, 
+                    preco,
+                    descricao
+                ) values (?,?,?)
+                `,
                 [
                     livro.titulo,
                     livro.preco,
@@ -23,9 +23,9 @@ class LivroDao {
                         console.log(err);
                         return reject('Não foi possível adicionar o livro!');
                     }
-    
+
                     resolve();
-                } 
+                }
             )
         });
     }
@@ -40,7 +40,71 @@ class LivroDao {
                     return resolve(resultados);
                 }
             )
+        });
+    }
 
+    buscaPorId(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro!');
+                    }
+                    return resolve(livro);
+                }
+            );
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ],
+            erro => {
+                if (erro) {
+                    return reject('Não foi possível atualizar o livro!');
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    remove(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    DELETE 
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro) => {
+                    if (erro) {
+                        return reject('Não foi possível remover o livro!');
+                    }
+                    return resolve();
+                }
+            );
         });
     }
 }
